1886ba8334cec576c5fe2ca39279eca713ed5bf9
jsnee <snoey.92@gmail.com>
2012-10-22 15:04:29 -0500

    Removed unused line and default comments from LoadConfig.cpp

--- AstroVis/LoadConfig.cpp
+++ AstroVis/LoadConfig.cpp
@@ -1,10 +1,3 @@
-//
-//  LoadConfig.cpp
-//  AstroVis
-//
-//  Created by  on 10/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
 
 #include <iostream>
 #include "globaldefs.h"
@@ -16,7 +9,6 @@ static char* getRoot(string fpath) {
     char * result;
     string root = (char*)getenv("HOME");
     string resultPath = (root).append(fpath);
-    //strcat(resultPath, fpath);
     result = new char [resultPath.size()+1];
     return strcpy(result,resultPath.c_str());
 }
